491854d5873960d96a4d4078e480e714ed5a8548
Nanoka <stornblood6969@gmail.com>
2024-07-17 05:57:07 -0500

    "Updated ReviewSlider component with new design and layout, and adjusted Reviews component and review.css styles accordingly."

--- src/components/ReviewSlider.jsx
+++ src/components/ReviewSlider.jsx
@@ -26,28 +26,28 @@ export default ({reviews = []}) => {
         breakpoints={breakpoints}
         modules={[Pagination]}
         className='h-96 w-full'
+        style={{ padding: ".5rem .5rem 0 .5rem" }}
       >
         {
-            reviews.map(({customer, raiting, review}, index) => {
-                const stars = Array(raiting).fill(0);
+            reviews.map(({customer, review}, index) => {
                 return (
                     <SwiperSlide key={index}>
-                        <div className="relative flex flex-col items-start">
-      <div className="relative bg-gray-800 text-white text-sm py-2 px-4 rounded-md ml-8 mt-2">
-        {review}
-        <div style={{
-            width: 0,
-            height: 0,
-            borderTop: "100px solid red",
-            borderRight: "100px solid transparent"
-
-        }}></div>
-      </div>
-      <div className="ml-2 mt-4 text-gray-800 font-bold bg-white p-1 rounded">
-        {customer}
-      </div>
-    </div>
-                </SwiperSlide>)
+                      <div style={{ 
+                        filter: "drop-shadow(-1px 6px 3px rgba(50, 50, 0, 0.5))",
+                        cursor: "col-resize"
+                       }}>
+                        <div className="bg-white w-full pb-12" style={{ 
+                          clipPath: 'polygon(0% 0%, 100% 0%, 100% 75%,40% 75%, 25% 90%, 25% 75%, 0% 75%)',
+                          }} >
+                          <div className='px-4 pt-4 pb-8'>
+                            <p className="line-clamp-[8] text-sm text-zinc-500">{review}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="ml-4 -mt-4 text-zinc-600 font-medium p-1 rounded">
+                        {customer}
+                      </div>
+                    </SwiperSlide>)
             })
         }
       </Swiper>
